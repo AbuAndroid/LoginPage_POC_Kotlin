ac3211905b8588cd237949a1801cab2299e43dae
Rashiff2022 <rashiff@digivalsolutions.com>
2022-12-23 19:37:33 +0530

    added new scrollableActivity

--- app/src/main/java/com/example/loginapplication/ScrollableActivity.kt
+++ app/src/main/java/com/example/loginapplication/ScrollableActivity.kt
@@ -5,20 +5,26 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.main.custom_toolbar.*
 
 class ScrollableActivity : AppCompatActivity() {
 
 
     private var uiAtLanguage:AutoCompleteTextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scrollable)
-        supportActionBar?.hide()
+
         setUpUi()
+
+        setSupportActionBar(toolbar)
+
+
     }
 
     private fun setUpUi() {
         uiAtLanguage = findViewById(R.id.uiActGnder)
+
         val languages = resources.getStringArray(R.array.str_array_programming_languages)
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, languages)
         this.uiAtLanguage?.setAdapter(arrayAdapter)
